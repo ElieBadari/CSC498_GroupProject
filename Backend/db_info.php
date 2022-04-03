7e201780e612dbe641a0d2a1ec5df90be02996aa
ElieBadari <elie.badari@hotmail.com>
2022-04-03 17:02:09 +0300

    first commit, linking local repo to github

---
+++ Backend/db_info.php
@@ -0,0 +1,19 @@
+<?php
+
+//this is to allow php to enter phpmyadmin page programatically 
+//we should never leave the username as root and password ass null (when the db is online/public)
+$db_host = "localhost"; //localhost because the db is hosted locally on my computer
+$db_user = "root";//user used to log into the db default is root
+$db_pass = null;//default = null  
+$db_name = "mobiledb";
+
+//constructor 
+$mysqli = new mysqli($db_host,$db_user,$db_pass,$db_name);
+//this php file can talk to the dbms using this php file because of the above line
+if(mysqli_connect_errno()){
+    die("connection failed");
+}
+
+
+
+?>
