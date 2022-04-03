598a8cf34738e7d06e1327bf92d134b2615ac54a
Raymond Saliba <raymondsaliba@Raymonds-MacBook-Pro.local>
2022-04-03 20:22:05 +0300

    Added a button with an intent that will take us to a second page

--- Frontend/CurrencyConverter2/app/src/main/java/com/example/converter2/MainActivity.java
+++ Frontend/CurrencyConverter2/app/src/main/java/com/example/converter2/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.converter2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void Start(View view){ // this function will take us from the cover page to the main page
+        Intent intent = new Intent(this,MainPage.class);
+        startActivity(intent);
+
     }
 }
