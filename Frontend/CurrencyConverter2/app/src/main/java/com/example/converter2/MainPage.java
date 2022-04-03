dc1c9f1e8e2e7763b941b6433bff71c7b0297e27
Raymond Saliba <raymondsaliba@Raymonds-MacBook-Pro.local>
2022-04-03 20:28:50 +0300

    Added a spinner with a choice of convertion

--- Frontend/CurrencyConverter2/app/src/main/java/com/example/converter2/MainPage.java
+++ Frontend/CurrencyConverter2/app/src/main/java/com/example/converter2/MainPage.java
@@ -3,6 +3,9 @@ package com.example.converter2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Spinner Convert = (Spinner) findViewById(R.id.spinner); // spinner that will let the user choose the convertion he wants
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(MainPage.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Convertion));
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Convert.setAdapter(myadapter);
     }
 }
